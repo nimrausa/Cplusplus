e0ecd03bcdc46389aaece76c6ebfe427ee8b073c
nimrausa <80575499+nimrausa@users.noreply.github.com>
2023-04-14 18:35:15 -0400

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+double  x;
+cout << "write number : ";
+    cin >> x;
+
+
+ if(x < 10 || x >20){
+ cout <<  "true" << endl;
+ }
+
+}
